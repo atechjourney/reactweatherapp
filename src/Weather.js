8db18a6781bc355be9b16d5b5aa9fd8a4848025f
atechjourney <alymariejo@gmail.com>
2023-02-07 21:57:46 -0400

    changing weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from 'axios';
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
+import Weatherforecast from "./WeatherForecast";
 
 
 export default function Weather(props) {
